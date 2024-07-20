1b78580e112095a371041e262fadfd96a4e9de8a
Kadir Paşaoğlu <92451463+kadirn7@users.noreply.github.com>
2024-07-20 23:37:58 +0300

    DataAccess  reflesh

---
+++ DataAccessLayer/Abstract/IBlogDal.cs
@@ -0,0 +1,20 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IBlogDal
+    {
+        List<Blog> ListAll();
+        void Add(Blog blog);
+        void Delete(Blog Blog);
+        void Update(Blog Blog);
+        
+        Blog GetbyId(int id);
+
+    }
+}
