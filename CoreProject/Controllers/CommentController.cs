4e5167760c7750c36bcd372705e626b4e968f80e
Kadir Paşaoğlu <92451463+kadirn7@users.noreply.github.com>
2024-08-10 07:10:46 +0300

    Comment pages update

---
+++ CoreProject/Controllers/CommentController.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreProject.Controllers
+{
+	public class CommentController : Controller
+	{
+		CommentManager _commentManager = new CommentManager(new EfCommentRepository());
+
+        public CommentController(CommentManager commentManager)
+        {
+            _commentManager = commentManager;
+        }
+
+        public IActionResult Index()
+		{
+			return View();
+
+		}
+		public PartialViewResult PartialAddComment()
+		{
+			return PartialView();
+		}
+		public PartialViewResult CommentListByBlog()
+		{
+			var values = _commentManager.Getlist();
+			return PartialView(values);
+		}
+	}
+}
