1e030e7152d9685d2e3643ae84263b979d6d6a0f
Kadir Paşaoğlu <92451463+kadirn7@users.noreply.github.com>
2024-08-16 07:13:50 +0300

    tema düzenlemeleri and  validation rules added project

---
+++ CoreProject/ViewComponents/Category/CategoryList.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreProject.ViewComponents.Category
+{
+    public class CategoryList:ViewComponent
+    {
+        CategoryManager _categoryManager =new CategoryManager(new EfCategoryRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values= _categoryManager.GetAllCategories();
+            return View(values);
+        }
+    }
+}
